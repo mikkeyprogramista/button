a1e3ec65ded81f6d276ceefd522864b8b10073de
mikkeyprogramista <mikkeyprogramista@gmail.com>
2026-01-29 11:44:59 +0100

    Add complete project

--- src/main/java/com/adz1q/button/Application.java
+++ src/main/java/com/adz1q/button/Application.java
@@ -3,6 +3,7 @@ package com.adz1q.button;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -11,7 +12,9 @@ public class Application extends javafx.application.Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Button");
+        stage.setTitle("Crazy Button");
+        stage.setResizable(false);
+        stage.initStyle(StageStyle.UNDECORATED);
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/adz1q/button/Controller.java
+++ src/main/java/com/adz1q/button/Controller.java
@@ -1,14 +1,50 @@
 package com.adz1q.button;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
 
-public class Controller {
-    @FXML
-    private Label welcomeText;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Pane pane;
+
+    @FXML
+    private Button btnClick;
+
+    @FXML
+    private Label lblInfo;
+
+    @FXML
+    private Button btnClose;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        btnClose.setOnAction(actionEvent -> Platform.exit());
+        btnClick.setOnAction(actionEvent -> {
+            btnClick.setText("Try again!");
+            lblInfo.setVisible(false);
+            generateRandomPosition(btnClick);
+        });
+        btnClose.setOnMouseEntered(actionEvent -> {
+            generateRandomPosition(btnClose);
+        });
+    }
+
+    private void generateRandomPosition(Button btn) {
+        double maxWidth = pane.getWidth() - btn.getWidth();
+        double maxHeight = pane.getHeight() - btn.getHeight();
+
+        double x = Math.random() * maxWidth;
+        double y = Math.random() * maxHeight;
+
+        btn.setLayoutX(x);
+        btn.setLayoutY(y);
     }
 }
